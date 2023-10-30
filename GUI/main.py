1993fe7bfa73097202a1553f6b355a3bc88406b7
Braini <duy30052001@gmail.com>
2023-10-30 16:48:18 +0700

    gui code
    khong co gi het

--- GUI/main.py
+++ GUI/main.py
@@ -1,83 +1,108 @@
 import sys
-from PySide6 import QtWidgets
-from PySide6.QtUiTools import QUiLoader
-from PySide6 import QtCore, QtGui
-from PySide6.QtWidgets import QPushButton
-import pyqtgraph as pg
-import math
-loader = QUiLoader()
-rowCount = 0
-app = QtWidgets.QApplication(sys.argv)
-window = loader.load("mainwindow.ui", None)
-#-------------------------------------------------------------------------------
-# function
-def do_something():
-    global rowCount    
-    window.table_data.setRowCount(rowCount)
-    window.table_data.insertRow(0)
-    window.table_data.setItem(0, 0, QtWidgets.QTableWidgetItem(window.roll_lineEdit.text()))
-    window.table_data.setItem(0, 1, QtWidgets.QTableWidgetItem(window.pitch_lineEdit.text()))
-    window.table_data.setItem(0, 2, QtWidgets.QTableWidgetItem(window.yaw_lineEdit.text()))
-    rowCount += 1   
+from PySide6 import QtWidgets, QtCore, QtGui
+from PySide6.QtCore import QPropertyAnimation
+from PyQt6 import uic
+from PySide6.QtCore import Qt
+from ui_menu import Ui_MainWindow
+
+
+class MainWindow(QtWidgets.QMainWindow):
     
+    def __init__(self):
+        super().__init__()
+        self.uic = Ui_MainWindow()
+        self.uic.setupUi(self)
 
-def pushButton():
-    window.setWindowTitle("Nothing")
-# -------------------------------------------------------------------
-# -----------------------------------------------------------
-# setup ui in started
-window.setWindowTitle("IMU-FMA")
-window.table_data.setColumnWidth(0,150)
-window.table_data.setColumnWidth(1,150)
-window.table_data.setColumnWidth(2,150)
-window.submit_button.clicked.connect(do_something)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setWindowOpacity(1)
 
+        self.uic.fr_sideMenu.setLineWidth(200)
 
-window.graphWidget = pg.PlotWidget(window.lineChartWidget)
-hour = [1,2,3,4,5,6,7,8,9,10]
-temperature = [30,32,34,32,33,31,29,32,35,45]
-window.graphWidget.setGeometry(QtCore.QRect(0, 0, 500, 300))
-window.graphWidget.setBackground('w')
-pen = pg.mkPen(color=(39, 164, 242), width=5)
-window.graphWidget.plot(hour, temperature, pen= pen)
-window.graphWidget.resize(500,255)
+        self.gripSize = 10
+        self.grip = QtWidgets.QSizeGrip(self)
+        self.grip.resize(self.gripSize, self.gripSize)
 
-hour1 = [1,2,3,4]
-temperature1 = [30,32,34,32]
-l_back = 400
-l_thigh = 400
-l_shin = 380
-l_foot = 200
-x0 = 0
-y0 = 0
-x1 = x0 + l_back*math.cos(math.radians(-90))
-y1 = y0 + l_back*math.sin(math.radians(-90))
-x2 = x1 + l_thigh*math.cos(math.radians(-40))
-y2 = y1 + l_thigh*math.sin(math.radians(-40))
-x3 = x2 + l_shin*math.cos(math.radians(-70))
-y3 = y2 + l_shin*math.sin(math.radians(-70))
-x4 = x3 + l_foot*math.cos(math.radians(-10))
-y4 = y3 + l_foot*math.sin(math.radians(-10))
-window.graphWidget2 = pg.PlotWidget(window.lineChartWidget)
-window.graphWidget2.setGeometry(QtCore.QRect(0, 260, 500, 300))
-window.graphWidget2.setBackground('w')
+        self.uic.fr_topMenu.mouseMoveEvent = self.mover 
 
-pen1 = pg.mkPen(color=(39, 164, 242), width=30)
-pen2 = pg.mkPen(color=(62, 174, 244), width=25)
-pen3 = pg.mkPen(color=(110, 194, 247), width=20)
-pen4 = pg.mkPen(color=(159, 215, 248), width=15)
-window.graphWidget2.setXRange(-100, 1500, padding=0)
-window.graphWidget2.setYRange(-1500, 100, padding=0)
+        self.uic.pb_Main.clicked.connect(lambda: self.uic.stackedWidget.setCurrentWidget(self.uic.page_Main))
+        self.uic.pb_Calibration.clicked.connect(lambda: self.uic.stackedWidget.setCurrentWidget(self.uic.page_Calib))
+        self.uic.pb_Introduction.clicked.connect(lambda: self.uic.stackedWidget.setCurrentWidget(self.uic.page_Introduction))
+        self.uic.pb_Patient.clicked.connect(lambda: self.uic.stackedWidget.setCurrentWidget(self.uic.page_PartientInfomation))
+        self.uic.pb_Review.clicked.connect(lambda: self.uic.stackedWidget.setCurrentWidget(self.uic.page_Review))
+        self.uic.pb_Setting.clicked.connect(lambda: self.uic.stackedWidget.setCurrentWidget(self.uic.page_Setting))
 
-window.graphWidget2.plot([x3,x4], [y3,y4], pen= pen4, symbol = 'o')
-window.graphWidget2.plot([x2,x3], [y2,y3], pen= pen3, symbol = 'o')
-window.graphWidget2.plot([x1,x2], [y1,y2], pen= pen2, symbol = 'o')
-window.graphWidget2.plot([x0,x1], [y0,y1], pen= pen1, symbol = 'o')
-window.graphWidget2.resize(500,500)
+        self.uic.bt_close.clicked.connect(self.control_bt_close)
+        self.uic.bt_expand.clicked.connect(self.control_bt_expand)
+        self.uic.bt_mini.clicked.connect(self.control_bt_mini)
+        self.uic.bt_smallSize.clicked.connect(self.control_bt_smallSize)
 
-#---------------------------------------------------------------
+        self.uic.bt_smallSize.hide()
 
+        self.uic.bt_Menu.clicked.connect(self.control_menu)
 
-window.show()
+    def control_bt_mini(self):
+        self.showMinimized()
+    
+    def control_bt_close(self):
+        self.close()
 
-app.exec()
+    def control_bt_expand(self):
+        self.showMaximized()
+        self.uic.bt_smallSize.show()
+        self.uic.bt_expand.hide()
+
+    def control_bt_smallSize(self, event):
+        self.showNormal()
+        if self.y() <=20:
+            self.move(200 , 20)        
+        self.uic.bt_smallSize.hide()
+        self.uic.bt_expand.show()
+
+    def control_menu(self):
+        width = self.uic.fr_sideMenu.width()
+        normal = 0
+        if width == 0:
+            expand_width = 200
+        else:
+            expand_width = normal
+        
+        self.animation = QPropertyAnimation(self.uic.fr_sideMenu, b'minimumWidth')
+        self.animation.setDuration(300)
+        self.animation.setStartValue(width)
+        self.animation.setEndValue(expand_width)
+        self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+        self.animation.start()
+
+    ## SizeGrip
+    def resizeEvent(self, event):
+        rect = self.rect()
+        self.grip.move(rect.right() - self.gripSize, rect.bottom() - self.gripSize)
+
+	## mover ventana
+    def mousePressEvent(self, event):
+        self.clickPosition = event.globalPosition()
+
+    def mover(self, event):
+        if self.isMaximized() == False:			
+            if event.buttons() == Qt.LeftButton:
+                delta = QtCore.QPointF(event.globalPosition() - self.clickPosition)
+                self.move(self.x() + delta.x(), self.y() + delta.y())
+                # self.move(self.pos() + event.globalPos() - self.clickPosition)
+                self.clickPosition = event.globalPosition()
+                event.accept()
+
+        if event.globalPosition().y() <=20:
+            self.showMaximized()
+            self.uic.bt_smallSize.show()
+            self.uic.bt_expand.hide()
+        else:
+            self.showNormal()
+            self.uic.bt_smallSize.hide()
+            self.uic.bt_expand.show()
+       
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec())
